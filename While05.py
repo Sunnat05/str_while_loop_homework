e9616a2ce6e97f61447749a18ff6957345462d88
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-19 22:09:07 +0500

    nth

--- While05.py
+++ While05.py
@@ -1,9 +1,17 @@
 def main(s):
-    """
+    ans=0
+    int=0
+    while int<len(s):
+        if s[int] in "qwertyuioplkjhgfdsazxcvbnm":
+            ans+=1
+        int+=1
+    
+                """
     A variable of type str is given. Find how many lowercase letters there are and return.
     Args:
         s: str
     Returns:
         int: return answer
     """
-    return
+    return ans
+print(main("dfdasd13e"))
