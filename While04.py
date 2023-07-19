8b349bfb1b6655087055db6e01e4151d4fd9c90f
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-19 22:03:02 +0500

    nth

--- While04.py
+++ While04.py
@@ -1,4 +1,10 @@
 def main(s):
+    int=0
+    ans=0
+    while int<len(s):
+        if s[int] in "QWERTYUIOPLKJHGFDSAZXCVBNM":
+            ans+=1
+        int+=1
     """
     A variable of type str is given. Find how many uppercase letters there are and return.
     Args:
@@ -6,4 +12,5 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return ans
+print(main("sadFD224f"))
