e59d565affddf03273ce917ba16535b9317adf3c
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-19 21:53:03 +0500

    nth

--- While02.py
+++ While02.py
@@ -1,4 +1,10 @@
 def main(s):
+    ind=0
+    ans=0
+    while ind<len(s):
+        if s[ind] in "qwertyuioplkjhgfdsazxcvbnm":
+            ans+=1
+        ind+=1 
     """
     A variable of type str is given. Find how many letters it contains and return.
     Args:
@@ -6,4 +12,5 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return ans
+print(main("ffee3rdjad3333334333"))
