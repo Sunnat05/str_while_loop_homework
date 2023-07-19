59dd7407515b1505c4c74c555682e6b5df392cf2
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-19 22:20:14 +0500

    nth

--- While07.py
+++ While07.py
@@ -1,4 +1,10 @@
 def main(s):
+    int=0
+    ans=0
+    while int<len(s):
+        if s[int] in "24680":
+            ans+=1
+        int+=1
     """
     A string of numbers is given. Find how many even numbers there are and return.
     Args:
@@ -6,4 +12,5 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return ans
+print(main("12345345676545"))
