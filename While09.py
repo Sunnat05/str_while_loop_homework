9b678d7548c2dd023cf1f95993556a96afe99fc4
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-19 23:06:37 +0500

    nth

--- While09.py
+++ While09.py
@@ -1,4 +1,10 @@
 def main(s):
+    ans=0
+    num=0
+    while num<len(s):
+        if s[num] in "0987654321":
+            ans+=int(s[num])
+        num+=1
     """
     A string of numbers is given. Find the sum of all the numbers and return.
     Args:
@@ -6,4 +12,5 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return ans
+print(main("334"))
